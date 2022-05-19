13bb47612a447ee898a762096a7021b08ef60eda
CJY114 <105726813+CJY114@users.noreply.github.com>
2022-05-19 21:39:38 +0900

    [Bronze V] Title: 심부름 가는 길, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/5554. 심부름 가는 길/심부름 가는 길.cc
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+	int num, sum = 0;
+
+	for (int i = 0; i < 4; i++)
+	{
+		scanf("%d", &num);
+		sum += num;
+	}
+
+	printf("%d\n", sum / 60);
+	printf("%d\n", sum % 60);
+}
